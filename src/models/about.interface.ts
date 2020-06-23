df07a5b238641cd0122897565250a48acc7e69e3
Cristian Andrei <brulea.andrei@yahoo.ro>
2020-06-23 11:37:58 +0300

    about us

---
+++ src/models/about.interface.ts
@@ -0,0 +1,4 @@
+export class IAboutUs
+{
+    public email!: string;
+}
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,51 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="body-color body">
+      <b-container class="body-container">
+        <div class="row about-us-label">
+          <div class="col-sm-4 col-md-4"><h4>About us</h4></div>
+          <div class="col-sm-4 col-md-4"></div>
+          <div class="col-sm-4 col-md-4"></div>
+        </div>
+        <div class="row about-us-label">
+          <div class="col-sm-4 col-md-4"><b>Email:</b> {{model.email}}</div>
+          <div class="col-sm-4 col-md-4"></div>
+          <div class="col-sm-4 col-md-4"></div>
+        </div>
+        <div class="row about-us-label">
+          <div class="col-sm-4 col-md-4"><b>Numar telefon:</b> {{model.email}}</div>
+          <div class="col-sm-4 col-md-4"></div>
+          <div class="col-sm-4 col-md-4"></div>
+        </div>
+      </b-container>
+
+      
+
+      
+    </div>
 </template>
+
+<style> 
+  .about-us-label 
+  {
+    text-align: left;
+    padding-top: 2vh !important;
+  }
+</style>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'; 
+import { IAboutUs } from '@/models/about.interface';
+ 
+@Component
+export default class AboutComponent extends Vue
+{  
+    
+    public model: IAboutUs = { email: "contact@homebank.com" };
+
+    back(){
+        this.$router.back();
+    }
+    
+}
+
+</script>
